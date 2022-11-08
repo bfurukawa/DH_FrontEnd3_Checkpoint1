96d4bea0929cf6b09269421bf7961aa414eccf78
Bruno Furukawa <bruno.b.furukawa@gmail.com>
2022-11-07 22:51:14 -0300

    Commit Inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-
+import {Card, useState, useSyncExternalStore} from 'react'
 // Aqui você irá escrever as suas funções de Validação, para verificar se o Formulário foi preenchido corretamente
 
 function App() {
   // Aqui você irá criar os Estados para manipular os Inputs
+  const [nome, setNome] = useState('')
+  const [cor, setCor] = useState('')
+  const [lista, setLista] = useState([])
 
+  function criaCard(e){
+    console.log(nome);
+    e.preventDefault();
+    const novoCard = {
+      corCard: cor,
+      nomeCArd: nome
+    }
+  }
 
   return (
     <div className="App">
      <h1>Carga de estudiantes</h1>
-     <form>
+     <form id="formulario" onSubmit={criaCard}>
+       <label htmlFor="nome">nome</label>
+       <input id="texto" type="text" value={nome} onChange={e=>setNome(nome+e.target.value)} />
+       <label htmlFor="cor">cor</label>
+       <input id="cor" type="text" value={cor} onChange={e=>setCor({cor: e.target.value})}/>
+       <input id="btn" type="submit" value="Salvar" />
       {/* Comece a desenvolver o seu Código por aqui :) */}
      </form>
     </div>
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,9 +1,10 @@
 //Este componente deverá receber dados por Props e mostrar as Informações em Tela
 
-export function Card () {
+export function Card (props) {
+  console.log(props.nome);
   return (
-    <div>
-
+    <div style={{color: props.a}}>
+      <h1>{props.div.formulario.nome}</h1>
     </div>
   )
 }
