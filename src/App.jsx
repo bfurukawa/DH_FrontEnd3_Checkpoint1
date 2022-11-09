bfd969f34b29ed09d886b457ac916f33247fdb2e
Bruno Furukawa <bruno.b.furukawa@gmail.com>
2022-11-09 18:55:40 -0300

    Commit funcional

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,100 @@
-import {Card, useState, useSyncExternalStore} from 'react'
+import {useState} from 'react'
+import {Card} from './Components/Card/Card.jsx'
 // Aqui você irá escrever as suas funções de Validação, para verificar se o Formulário foi preenchido corretamente
 
 function App() {
   // Aqui você irá criar os Estados para manipular os Inputs
+  const [erroInput, setErroInput] = useState(false)
+  const [msgErro, setMsgErro] = useState([])
   const [nome, setNome] = useState('')
   const [cor, setCor] = useState('')
-  const [lista, setLista] = useState([])
+  const [listaCores, setListaCores] = useState([
+    {nome:'Azul',cor:'#0000FF'}
+  ])
 
+  //useEffect(()=>{console.log('use effect ran')},[erroInput,msgErro])
+
+  function setCallback(value,setFunction){
+    setFunction(value)
+  }
+
+  function resetErros(){
+    setCallback('',setMsgErro)
+    setCallback(false,setErroInput)
+  }
+
+
+  //Funcao recebendo o subnmit do dormulario
   function criaCard(e){
-    console.log(nome);
     e.preventDefault();
-    const novoCard = {
-      corCard: cor,
-      nomeCArd: nome
-    }
+
+    //criamos variaveis locais que posteriormente serao enviadas ao contexto global
+    let erroValidacao = false;
+    let mensagemErro = [];
+
+    //variavbel tempcorCriada para facilitar o desmembramento e trabalhar com a string
+    let tempCor = cor
+
+    
+
+    //Resetamos a variavel booleana usada para identificad a presenca de erros
+    //E resetamos a msg de erro a ser apresentada ao usuario
+     if(nome.length == 0 || (nome.length<=3) || (/\d/.test(nome))){
+       erroValidacao = true
+       mensagemErro.push('O nome contem menos que 3 caracteres ou contem numeros')
+      }
+
+     if(tempCor.slice(0,1)!='#'){
+       tempCor='#'+tempCor
+     }
+     if(tempCor.slice(1).length<0 ||tempCor.slice(1).length<6 || !(/\d/.test(tempCor.slice(1)))){
+        erroValidacao = true
+        mensagemErro.push('A cor deve ser apresentada no formato hexadecimal ex:#0000FF')
+        mensagemErro.push('A definicao da cor contem menos que 6 caracteres ou nao contem numeros')
+      }
+
+     if(!erroValidacao){
+       const novaCor = {
+       nome:nome,
+       cor:tempCor
+     }
+     setListaCores([...listaCores,novaCor])
+     }
+     setMsgErro(mensagemErro)
+     setErroInput(erroValidacao)
   }
 
   return (
+    <>
     <div className="App">
      <h1>Carga de estudiantes</h1>
+     {erroInput ? msgErro.map((itemMsgErro)=>{
+       return <p>{itemMsgErro}</p>
+     }) : ''}
      <form id="formulario" onSubmit={criaCard}>
-       <label htmlFor="nome">nome</label>
-       <input id="texto" type="text" value={nome} onChange={e=>setNome(nome+e.target.value)} />
-       <label htmlFor="cor">cor</label>
-       <input id="cor" type="text" value={cor} onChange={e=>setCor({cor: e.target.value})}/>
-       <input id="btn" type="submit" value="Salvar" />
-      {/* Comece a desenvolver o seu Código por aqui :) */}
+       <div>
+        <label htmlFor="nome">nome</label>
+        <input id="texto" type="text" onChange={e=>setNome(e.target.value)} />
+       </div>
+       <div>
+        <label htmlFor="cor">cor</label>
+        <input id="cor" type="cor" onChange={e=>setCor(e.target.value)}/>
+       </div>
+       
+       <input id="btn" type="submit" />
      </form>
     </div>
+    <div style={{
+      'display':'flex',
+      'flex-direction':'row',
+      'flex-wrap': 'wrap'
+    }}>
+      {listaCores.map((itemListaCor)=>{return(
+        <Card corCard={itemListaCor} />
+      )})}
+    </div>
+    </>
+    
   )
 }
 
